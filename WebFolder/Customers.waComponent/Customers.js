3920edfc4eb61e81db6a98ef82c9ac72a3e1d271
Dirk <dneumann42@gmail.com>
2012-05-03 18:13:28 +0200

    confirm p1

--- WebFolder/Customers.waComponent/Customers.js
+++ WebFolder/Customers.waComponent/Customers.js
@@ -13,6 +13,9 @@ function constructor (id) {
 	this.load = function (data) {// @lock
 
 	// @region namespaceDeclaration// @startlock
+	var customerDeleteButton = {};	// @button
+	var button9 = {};	// @button
+	var button8 = {};	// @button
 	var saveCustomerButton = {};	// @button
 	// @endregion// @endlock
 	$$(getHtmlId("saveCustomerButton")).disable();
@@ -49,6 +52,23 @@ function constructor (id) {
     	}
 	// eventHandlers// @lock
 
+	customerDeleteButton.click = function customerDeleteButton_click (event)// @startlock
+	{// @endlock
+		var name = sources.customer.name;
+		console.log(name);
+		$$(getHtmlId('dialog1Text')).setValue('Really delete ' + name + ' ?');
+	};// @lock
+
+	button9.click = function button9_click (event)// @startlock
+	{// @endlock
+		$$('dialog1').closeDialog(); //cancel button
+	};// @lock
+
+	button8.click = function button8_click (event)// @startlock
+	{// @endlock
+		$$('dialog1').closeDialog(); //ok button
+	};// @lock
+
 	saveCustomerButton.click = function saveCustomerButton_click (event)// @startlock
 	{// @endlock
 		sources.customer.save({
@@ -64,6 +84,9 @@ function constructor (id) {
 	};// @lock
 
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_customerDeleteButton", "click", customerDeleteButton.click, "WAF");
+	WAF.addListener(this.id + "_button9", "click", button9.click, "WAF");
+	WAF.addListener(this.id + "_button8", "click", button8.click, "WAF");
 	WAF.addListener(this.id + "_saveCustomerButton", "click", saveCustomerButton.click, "WAF");
 	// @endregion// @endlock
 
