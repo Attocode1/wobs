33e45b5965f52b9712296d643d335c10623869cb
Dirk Neumann <dneumann42@gmail.com>
2012-07-09 17:38:39 +0200

    title field

--- WebFolder/scripts/index.js
+++ WebFolder/scripts/index.js
@@ -2,10 +2,21 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var menuItemContacts = {};	// @menuItem
 // @endregion// @endlock
 
 // eventHandlers// @lock
 
+	menuItemContacts.click = function menuItemContacts_click (event)// @startlock
+	{// @endlock
+		WAF.loadComponent ( {
+        id:     'component1',
+        path:   '/Contacts.waComponent'
+     } );
+     $$('title').setValue('Contacts');
+	};// @lock
+
 // @region eventManager// @startlock
+	WAF.addListener("menuItemContacts", "click", menuItemContacts.click, "WAF");
 // @endregion
 };// @endlock
